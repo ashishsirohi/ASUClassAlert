5edc5c2c97e983d046e33ddbdded7b0e74a945d3
Ashish <ashishsirohi@live.com>
2017-07-03 17:31:47 -0700

    Minor change in Settings.py

--- asuclassalert/settings.py
+++ asuclassalert/settings.py
@@ -145,6 +145,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
