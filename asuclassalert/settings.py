b5d6cd0aea7968630173b0afe38568c05ba8f67a
Ashish <ashishsirohi@live.com>
2017-07-03 19:03:19 -0700

    DB Config changes for heroku server

--- asuclassalert/settings.py
+++ asuclassalert/settings.py
@@ -90,10 +90,12 @@ DATABASES = {
 'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'asuca',
-        'USER': 'admin',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'USER': 'ltzxrbwxeflttl',
+        'PASSWORD': 'e41823271e2a9ac436ace68717dd9cd8542527510e61dcf5fbcbf36c07786e91',
+        'HOST': 'ec2-54-225-68-71.compute-1.amazonaws.com',
         'PORT': '5432',
+        'Database': 'd1ep6vi51si3e6'
+        'URI': 'postgres://ltzxrbwxeflttl:e41823271e2a9ac436ace68717dd9cd8542527510e61dcf5fbcbf36c07786e91@ec2-54-225-68-71.compute-1.amazonaws.com:5432/d1ep6vi51si3e6'
     }
 }
 
